de71e09a05a46296e1b0b9b655af3f06f807a22c
udit <udit>
2024-12-27 18:07:20 +0530

    Initial commit

--- src/components/settings/Profile.jsx
+++ src/components/settings/Profile.jsx
@@ -12,8 +12,8 @@ const Profile = () => {
 				/>
 
 				<div>
-					<h3 className='text-lg font-semibold text-gray-100'>John Doe</h3>
-					<p className='text-gray-400'>john.doe@example.com</p>
+					<h3 className='text-lg font-semibold text-gray-100'>udit shah</h3>
+					<p className='text-gray-400'>udit.m.shah@gmail.com</p>
 				</div>
 			</div>
 
